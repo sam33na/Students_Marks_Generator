7b3a235e274340ea9c8ad3c1978d11aed7c50abe
sam33na <saminashrestha98@gmail.com>
2020-12-26 14:27:54 +0545

    student class

--- app/src/main/java/com/marks/Student.kt
+++ app/src/main/java/com/marks/Student.kt
@@ -1,2 +1,12 @@
-package com.marks 
+package com.marks
 
+import java.io.Serializable
+
+class Student(val id: Int, val name: String, val markAndroid: Double,
+              val markAPI: Double, val markIOT: Double): Serializable{
+//    constructor(): this(3, "s", 50.3, 50.2, 50.5)
+
+    fun percentageObtained(): Double{
+        return String.format("%.3f", (markAndroid + markAPI + markIOT) / 3).toDouble()
+    }
+}
--- app/src/main/java/com/marks/myapplication/StudentsMarknActivity.kt
+++ app/src/main/java/com/marks/myapplication/StudentsMarknActivity.kt
@@ -1,25 +1,71 @@
 package com.marks.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import com.marks.Student
 
-class StudentsMarknActivity : AppCompatActivity() {
+class StudentsMarknActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var stdId:EditText
     private lateinit var stdName:EditText
-    private lateinit var androd:EditText
-    private lateinit var api:EditText
-    private lateinit var iot:EditText
-    private lateinit var btn:EditText
+    private lateinit var androidM:EditText
+    private lateinit var apiM:EditText
+    private lateinit var iotM:EditText
+    private lateinit var studentAdd:EditText
+
+    //creating list for storing students data
+    private lateinit var studentList:MutableList<Student>
+    private lateinit var student:Student
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_marks)
 
         stdId=findViewById(R.id.stdId)
         stdName=findViewById(R.id.stdName)
-        androd=findViewById(R.id.androidMark)
-        api=findViewById(R.id.apiMark)
-        iot=findViewById(R.id.iotMark)
-        btn=findViewById(R.id.btnList)
+        androidM=findViewById(R.id.androidMark)
+        apiM=findViewById(R.id.apiMark)
+        iotM=findViewById(R.id.iotMark)
+        studentAdd=findViewById(R.id.btnList)
+
+        studentList=ArrayList()
+        studentAdd.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?)
+    {
+        //assigning an instance of class 'Student' to variable 'student' i.e. creating object
+        student = Student(stdId.text.toString().toInt(), stdName.text.toString(),
+                androidM.text.toString().toDouble(), apiM.text.toString().toDouble(),
+                iotM.text.toString().toDouble())
+//            student = Student()
+        studentList.add(student)    //adding the most recent 'student' object to list
+
+        //altering the button text
+        if (studentList.size == 2){
+            studentAdd.text = "Add and Show All"
+        }
+        //opening another activity once the list size reaches 3
+        else if (studentList.size == 3){
+            val displayResultIntent = Intent(this, ResultActivity::class.java).also {
+                it.putExtra("studentList", studentList as Serializable?)    //passing list of object
+            }
+            startActivity(displayResultIntent)
+            //assigning the button with its actual text and clearing the values in list
+            studentAdd.text = getString(R.string.student_Add)
+            studentList.clear()
+        }
+        //clearing all the editText for each button click
+        clearAll()
+    }
+
+private fun clearAll(){
+    stdId.setText("")
+    stdName.setText("")
+    androidM.setText("")
+    apiM.setText("")
+    iotM.setText("")
+    stdId.requestFocus()
+}
 }
